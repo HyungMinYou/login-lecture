f785349cec8de5c9efb9f9db68d0bffc2b848961
HyungMin Yu <hyungminyu@HyungMinui-MacBookPro.local>
2022-12-08 16:36:09 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -11,6 +11,10 @@ const home = require("./src/routes/home");
 app.set("views", "./src/views"); // 6. 밑 html<DOCK> 부분 views 경로에 뷰단 처리 엔진
 app.set("view engine", "ejs"); // 6. html코드들을 어떤 엔진으로 해석할지 정함
 
+app.use(express.static(`${__dirname}/src/public`));
+// ejs파일에서 js폴더로 접근할 수 있도록 미들웨어 등록
+// js폴더를 public으로 옮김
+
 app.use("/", home); // use -> 미들 웨어를 등록해주는 메서드
 // index.js안에서 외부로 보낸 라우터를 app.use를 이용해 받아옴
 
